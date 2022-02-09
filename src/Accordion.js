5a801e8590109e9437a96518f2e2edfe2ed27436
Kisich <94457745+frankmombo@users.noreply.github.com>
2022-02-09 21:08:28 +0300

    widgets commit

--- src/Accordion.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Accordion = () => {
-  return <h1>Accordion</h1>;
-};
-
-export default Accordion;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import Accordion from './Accordion';
+import Accordion from './components/Accordion';
 
+const items = [
+  {
+    title: 'what is React?',
+    content: 'React is a front end javascript Framework',
+  },
+  {
+    title: 'why use react',
+    content: 'React is a favorite JS library among engineers',
+  },
+  {
+    title: 'HoW do you use React?',
+    content: 'you use React by creating components',
+  },
+];
 export default () => {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items} />
     </div>
   );
 };
